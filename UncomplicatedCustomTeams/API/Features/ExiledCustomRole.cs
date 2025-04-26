97deb51abbb534a7f0f2c6d3a5fb8a6407b565c1
icedchai <70030002+icedchai@users.noreply.github.com>
2025-04-26 14:58:38 -0500

    Small changes

--- UncomplicatedCustomTeams/API/Features/ExiledCustomRole.cs
+++ UncomplicatedCustomTeams/API/Features/ExiledCustomRole.cs
@@ -7,18 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UncomplicatedCustomTeams.API.Enums;
+using YamlDotNet.Serialization;
 
 namespace UncomplicatedCustomTeams.API.Features
 {
     public class ExiledCustomRole : CustomRole
     {
+        [YamlIgnore]
         private Exiled.CustomRoles.API.Features.CustomRole CustomRole => Exiled.CustomRoles.API.Features.CustomRole.Get((uint)Id);
         public int MaxPlayers { get; set; }
         public RolePriority Priority { get; set; } = RolePriority.First;
         public int Id { get; set; }
 
+        [YamlIgnore]
         public string Name => CustomRole.Name;
 
+        [YamlIgnore]
         public RoleTypeId Role => CustomRole.Role;
 
         public void Spawn(Player player)
