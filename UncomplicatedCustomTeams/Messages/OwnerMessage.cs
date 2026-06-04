73eaf3b72092d329124be8079a65cf1f3eaa2805
.piwnica <piwnica.mirai@gmail.com>
2026-06-04 14:13:41 +0200

    HTTP update

---
+++ UncomplicatedCustomTeams/Messages/OwnerMessage.cs
@@ -0,0 +1,14 @@
+﻿using LabApi.Features.Wrappers;
+using System.Text.Json.Serialization;
+
+namespace UncomplicatedCustomTeams.Messages
+{
+    internal class OwnerMessage(Player player, string discordId)
+    {
+        [JsonPropertyName("user_id")]
+        public string UserId { get; set; } = player.UserId;
+
+        [JsonPropertyName("discord_id")]
+        public string DiscordId { get; set; } = discordId;
+    }
+}
---
+++ UncomplicatedCustomTeams/Messages/ShareLogMessage.cs
@@ -0,0 +1,21 @@
+﻿using LabApi.Features;
+using System.Text.Json.Serialization;
+using UncomplicatedCustomTeams.Manager;
+
+namespace UncomplicatedCustomTeams.Messages
+{
+    internal class ShareLogMessage(string message)
+    {
+        [JsonPropertyName("labapi_version")]
+        public string LabAPIVersion { get; set; } = LabApiProperties.CompiledVersion;
+
+        [JsonPropertyName("plugin_version")]
+        public string PluginVersion { get; set; } = Plugin.Singleton.Version.ToString(4);
+
+        [JsonPropertyName("hash")]
+        public string Hash { get; set; } = VersionManager.HashFile(Plugin.Singleton.FilePath);
+
+        [JsonPropertyName("message")]
+        public string Message { get; set; } = message;
+    }
+}
